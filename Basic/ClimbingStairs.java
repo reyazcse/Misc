094bf0d1789d3fe3a38b9d56d98af5f3ddaa5cc4
reyazcse <57748074+reyazcse@users.noreply.github.com>
2020-05-03 13:19:47 +0530

    Add files via upload

---
+++ Basic/ClimbingStairs.java
@@ -0,0 +1,20 @@
+//easy
+//src: https://leetcode.com/problems/climbing-stairs/
+package leetcode;
+
+public class ClimbingStairs {
+	public int climbStairs(int n) {
+        int[]dp  =new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+        
+    }
+	public static void main(String[] args) {		
+
+	}
+
+}
